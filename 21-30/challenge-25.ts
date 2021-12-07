0d19fd8217bc4c0628b41753ccd67a7c82cd1057
ahfaisalkarimi <ahfaisalkarimi@gmail.com>
2021-12-07 11:42:24 -0800

    challenge 25

---
+++ 21-30/challenge-25.ts
@@ -0,0 +1,22 @@
+// SELECTION SORT
+
+const arr3 = [10, 2, 3, 9, 7, 6, 5, 8, 4, 1];
+
+const selectionSort = (arr: number[]) => {
+  for (let i = 0; i < arr.length; i++) {
+    let min = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[min]) {
+        min = j;
+      }
+    }
+    if (min !== i) {
+      const temp = arr[i];
+      arr[i] = arr[min];
+      arr[min] = temp;
+    }
+  }
+  return arr;
+}
+
+console.log(selectionSort(arr3));
