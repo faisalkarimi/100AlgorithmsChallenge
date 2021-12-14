32f2d7648c3cae87eb14bbe983a2bc53034fbe9e
ahfaisalkarimi <ahfaisalkarimi@gmail.com>
2021-12-14 11:17:50 -0800

    challenge 56

---
+++ 51-60/challenge-56.ts
@@ -0,0 +1,17 @@
+// MERGE TWO SORTED LIST
+
+const mergeTwoSortedList = (l1: ListNode | null, l2: ListNode | null): ListNode | null => {
+  if (l1 === null) {
+    return l2;
+  }
+  if (l2 === null) {
+    return l1;
+  }
+  if (l1.val < l2.val) {
+    l1.next = mergeTwoSortedList(l1.next, l2);
+    return l1;
+  } else {
+    l2.next = mergeTwoSortedList(l1, l2.next);
+    return l2;
+  }
+}
